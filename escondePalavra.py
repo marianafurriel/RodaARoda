7c3b24c1cf2ae78f2cba4125913627cbe4e35ec4
Mariana Furriel <marianafurrielss@gmail.com>
2022-06-22 00:16:49 -0300

    Sorteia temas, sorteia item, esconde palavra e revela letra implementados

---
+++ escondePalavra.py
@@ -0,0 +1,4 @@
+def escondePalavra(palavra):
+    """Recebe uma palavra e retorna a palavra escondida com underline (_)"""
+    palavraEscondida = '_'*len(palavra)
+    return palavraEscondida
---
+++ revelaLetra.py
@@ -0,0 +1,10 @@
+def revelaLetra(palavra, palavraOculta, letra):
+    """Recebe 3 strings: uma palavra, a palavra oculta e uma letra. Retorna a palavra oculta com apenas a letra revelada em todas as posições que ela estiver na palavra."""
+    if letra in palavra:
+        aux = palavra.count(letra)
+        for i in range(aux):
+            indice = palavra.index(letra)
+            palavraOculta = palavraOculta[:indice] +
+            letra + palavraOculta[indice+1:]
+            palavra = palavra[:indice]+"$"+palavra[indice+1:]
+    return palavraOculta
