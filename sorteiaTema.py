f59c1089d78acf52d4e0696ec6f95272e737ebfb
Mariana Furriel <marianafurrielss@gmail.com>
2022-06-21 23:11:53 -0300

    init

---
+++ sorteiaTema.py
@@ -0,0 +1,18 @@
+import random as r
+
+def sorteiaTema(dicTemas):
+    """Recebe um dicionario e retorna uma lista no formato [tema, palavra, palavra, palavra] com um tema
+    aleatório contido no dicionário recebido e palavras do respectivo tema."""
+    #dicionario = {'Animais':['cachorro','gato','peixe'],'Cores':['azul','vermelho','amarelo']}
+    tema = r.choice(list(dicTemas))
+    palavras = dicTemas.get(tema)
+    indicesPalavras = []
+    while(True):
+        numeroAleatorio = r.randint(0, len(palavras)-1)
+        if numeroAleatorio not in indicesPalavras:
+            indicesPalavras = indicesPalavras + [numeroAleatorio]
+        if len(indicesPalavras) == 3:
+            break
+    retorno = [tema, palavras[indicesPalavras[0]],
+               palavras[indicesPalavras[1]], palavras[indicesPalavras[2]]]
+    return retorno
