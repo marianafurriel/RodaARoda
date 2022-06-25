59203d8f1ee32975e5e2cef6ee769778951e60b5
Mariana Furriel <marianafurrielss@gmail.com>
2022-06-24 23:20:36 -0300

    Tabuleiro iniciado

--- tabuleiro.py
+++ tabuleiro.py
@@ -3,5 +3,5 @@ def tabuleiro(pontos, jogador, roleta, pontosAtuais, tema, p1, p2, p3, letrasErr
         f"+= == == == == == == == == == == == == == == == == == == == == == == ===\n| RODADA X - TURNO Y\n+= == == == == == == == == == == == == == == == == == == == == == == ===\n| ANA - {pontos['Ana']} | BARBARA - {pontos['Bárbara']} | CARLOS - {pontos['Carlos']}\n+= == == == == == == == == == == == == == == == == == == == == == == ===\nJogador ativo: {jogador}\nPontuação atual: {pontos[jogador]}>\nRoleta: {roleta}\nNova pontuacao: {pontosAtuais}\n+= == == == == == == == == == == == == == == == == == == == == == == ===\nTema: {tema}\nP1) {p1}\nP2){p2}\nP3){p3}\nLetras erradas: {letrasErradas}\n+= == == == == == == == == == == == == == == == == == == == == == == == =")
 
 
-tabuleiro({'Ana': 100, 'Bárbara': 150, 'Carlos': 0}, 'Ana', '100',
+#tabuleiro({'Ana': 100, 'Bárbara': 150, 'Carlos': 0}, 'Ana', '100',
           300, 'Animais', '_ a _', '_ a _ _', '_ a _ a _ _', ['z', 'h'])
